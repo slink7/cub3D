5148846f689c58dc29c65391ae99310d1b2a594b
slink7 <slink794@gmail.com>
2025-12-01 01:52:44 +0100

    :building_construction: Changed data structures

--- include/cub3D.h
+++ include/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 19:10:01 by scambier          #+#    #+#             */
-/*   Updated: 2025/11/18 22:51:27 by scambier         ###   ########.fr       */
+/*   Updated: 2025/11/30 19:14:37 by scambier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,29 +20,53 @@
 #define B 2
 #define A 3
 
+typedef struct s_image
+{
+	void			*mlx_img;
+	char			*data;
+	unsigned int	width;
+	unsigned int	height;
+	unsigned int	endian;
+	unsigned int	bpp;
+}	t_image;
+
+#define ALBEDO 0
+#define NORMAL 1
+
+typedef struct s_texture
+{
+	char			*path;
+	t_image			source;
+	t_image			*images[2];
+	unsigned int	image_count;
+}	t_texture;
+
+
 typedef union u_color
 {
 	int		hexa;
 	char	rgba[4];
 }	t_color;
 
-typedef union u_display_data
-{
-	t_color	color;
-	char	*path;
-}	t_display_data;
 
-typedef enum e_display_type
+typedef union u_surface_data
+{
+	t_color		color;
+	t_texture	*texture;
+}	t_surface_data;
+
+
+typedef enum e_surface_type
 {
 	COLOR,
-	PATH
-}	t_display_type;
+	TEXTURE,
+}	t_surface_type;
 
-typedef struct s_display
+typedef struct s_surface
 {
-	t_display_data	data;
-	t_display_type	type;
-}	t_display;
+	t_surface_data	data;
+	t_surface_type	type;
+}	t_surface;
 
 #define NORTH	0
 #define EAST	1
@@ -53,7 +77,7 @@ typedef struct s_display
 
 typedef struct s_map
 {
-	t_display	displays[6];
+	t_surface	surfaces[6];
 	char		**tiles;
 }	t_map;
 
--- sources/debug.c
+++ sources/debug.c
@@ -1,11 +1,36 @@
 #include "libft.h"
 #include "cub3D.h"
 
-void	print_display(t_display *d)
+void	print_image(t_image *image, char *name)
 {
-	if (d->type == COLOR) {
-		ft_printf("Type Color: %d (%d, %d, %d)\n", d->data.color.hexa, d->data.color.rgba[R], d->data.color.rgba[G], d->data.color.rgba[B]);
-	} else if (d->type == PATH) {
-		ft_printf("Type Path: %s\n", d->data.path);
+
+	if (image && image->mlx_img)
+		ft_printf("loaded %s image (%u, %u)\n", name, image->width,
+			image->height);
+	else
+		ft_printf("unloaded %s image\n", name);
+}
+
+void	print_surface(t_surface *surface)
+{
+	t_texture		*texture;
+	unsigned int	k;
+
+	if (surface->type == COLOR)
+		ft_printf("Color: %#08x\n", surface->data.color.hexa);
+	else
+	{
+		texture = surface->data.texture;
+		ft_printf("Texture path: %s\n", texture->path);
+		print_image(&texture->source, "source");
+		if (!texture->image_count)
+			return ;
+		ft_printf("%u sub images\n", texture->image_count);
+		k = -1u;
+		while (++k < texture->image_count)
+		{
+			print_image(&texture->images[k][ALBEDO], "albedo");
+			print_image(&texture->images[k][NORMAL], "normal");
+		}
 	}
 }
