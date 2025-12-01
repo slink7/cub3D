d07ec04e9037aa22abbb65d9f54ddda1669eba80
slink7 <slink794@gmail.com>
2025-12-01 23:16:48 +0100

    :recycle: Refactored load_map_header.c to use libft color functions

--- sources/parsing/load_map_header.c
+++ sources/parsing/load_map_header.c
@@ -6,14 +6,17 @@
 /*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 15:22:55 by scambier          #+#    #+#             */
-/*   Updated: 2025/11/24 15:27:24 by scambier         ###   ########.fr       */
+/*   Updated: 2025/12/01 02:00:48 by scambier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
 #include "libft.h"
+#include "color.h"
 #include "cub3D.h"
+
+t_texture	*create_texture(char *path);
 
 t_err	extract_var(t_bst **bst, char *line)
 {
@@ -59,35 +62,21 @@ t_err	read_map_header(t_bst **infos, char **lines, int *line_index)
 	return (0);
 }
 
-t_err	load_display(t_display *out, char *in)
+t_err	load_surface(t_surface *out, char *in)
 {
-	char	**rgb;
-
-	if (ft_strncmp(in, "0x", 2) == 0)
-	{
+	if (ft_atoc(&out->data.color, in))
 		out->type = COLOR;
-		out->data.color.hexa = ft_atoi_base(in, "0123456789abcdef");
-	}
-	else if (ft_isdigit(in[0]))
-	{
-		out->type = COLOR;
-		rgb = ft_split(in, ',');
-		if (!rgb)
-			return (ERR_MALLOC);
-		if (ft_strarrlen(rgb) != 3)
-			return (ft_strarrfree(rgb), 0 & ft_printf("Invalid color '%s'", in));
-		out->data.color.rgba[R] = ft_atoi(rgb[0]) % 256;
-		out->data.color.rgba[G] = ft_atoi(rgb[1]) % 256;
-		out->data.color.rgba[B] = ft_atoi(rgb[2]) % 256;
-		ft_strarrfree(rgb);
-	}
 	else
 	{
-		out->type = PATH;
-		out->data.path = in;
+		out->type = TEXTURE;
+		out->data.texture = create_texture(in);
+		if (!out->data.texture)
+			return (ERR_MALLOC);
 	}
 	return (0);
 }
+
+void	print_surface(t_surface *surf);
 
 t_err	parse_map_header(t_map *map, t_bst *infos)
 {
@@ -104,13 +93,13 @@ t_err	parse_map_header(t_map *map, t_bst *infos)
 	char			*value;
 
 	(void) error;
-	k = -1;
+	k = -1u;
 	while (++k < (sizeof(required_var) / sizeof(char *)))
 	{
 		value = ft_bst_getval(infos, required_var[k]);
 		if (!value)
 			return (ERR_HEADER & ft_printf("Warning: Missing '%s'", required_var[k]));
-		load_display(&map->displays[k], value);
+		load_surface(&map->surfaces[k], value);
 	}
 	return (0);
 }
@@ -127,5 +116,6 @@ t_err	handle_map_header(t_map *map, char **lines, int *line_index)
 	err = parse_map_header(map, infos);
 	if (err)
 		return (ft_bst_free(&infos), err);
+	ft_bst_print(infos);
 	return (ft_bst_free(&infos), 0);
 }
